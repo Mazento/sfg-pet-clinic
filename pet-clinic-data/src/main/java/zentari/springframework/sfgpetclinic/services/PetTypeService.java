c3a23f518cd362e7bee4431b888704572e0665f5
Zentari <zenti.asteris@gmail.com>
2020-12-02 04:42:44 +0300

    Add petType and specialty entities with maps

---
+++ pet-clinic-data/src/main/java/zentari/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package zentari.springframework.sfgpetclinic.services;
+
+import zentari.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/zentari/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package zentari.springframework.sfgpetclinic.services;
+
+import zentari.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
