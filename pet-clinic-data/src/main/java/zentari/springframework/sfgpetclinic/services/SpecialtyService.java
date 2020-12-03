77648dd282c3afc4dd58c9fab2f956232a270db3
Zentari <zenti.asteris@gmail.com>
2020-12-03 18:41:06 +0300

    Add new init data, fix broken links

--- pet-clinic-data/src/main/java/zentari/springframework/sfgpetclinic/services/SpecialtyService.java
+++ pet-clinic-data/src/main/java/zentari/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -2,5 +2,5 @@ package zentari.springframework.sfgpetclinic.services;
 
 import zentari.springframework.sfgpetclinic.model.Specialty;
 
-public interface SpecialtiesService extends CrudService<Specialty, Long> {
+public interface SpecialtyService extends CrudService<Specialty, Long> {
 }
